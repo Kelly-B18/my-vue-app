5f864b7127192205214f901bd53e0690027a75a6
Kelly <kellybanog@gmail.com>
2026-09-11 10:29:44 +0800

    fix case

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
+import About from '@/views/about.vue'
 import Admissions from '@/views/Admissions.vue'
 import Academics from '@/views/Academics.vue'
 import CampusLife from '@/views/CampusLife.vue'
