81e4d147fda8d4b52665488def0657a2ba39a3b9
Kelly <kellybanog@gmail.com>
2026-09-11 20:32:19 +0800

    update new code in sample model

---
+++ src/views/SampleVmodel.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      age: null,
+      course: ""
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="form-container">
+    <!-- Form Inputs with v-model -->
+    <input type="text" v-model="name" placeholder="Enter Name"><br><br>
+    <input type="number" v-model="age" placeholder="Enter Age"><br><br>
+    <input type="text" v-model="course" placeholder="Enter Course"><br><br>
+
+    <hr>
+
+    <!-- Live Display updating instantly -->
+    <p>Name: {{ name }}</p>
+    <p>Age: {{ age }}</p>
+    <p>Course: {{ course }}</p>
+  </div>
+</template>
